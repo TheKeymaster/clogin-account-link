c37809a9146c16cf81bc4039057cb47bca6d04e8
Dominik Brader <dominik@brader.co.at>
2021-10-05 11:53:30 +0200

    Create clogin-account-link.js

---
+++ clogin-account-link.js
@@ -0,0 +1,31 @@
+// ==UserScript==
+// @name         Account link in clogin
+// @namespace    https://www.findologic.com/
+// @version      0.1.0
+// @description  Quick link inside clogin to go to the Account
+// @author       TheKeymaster
+// @match        https://secure.findologic.com/login.symfony/web/app.php/*
+// @icon         https://www.google.com/s2/favicons?domain=findologic.com
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+
+    const assumeButton = document.querySelector('a.btn.btn-large.btn-assume.btn-inverse');
+    if (!assumeButton) {
+        return;
+    }
+
+    const linkParts = assumeButton.href.split('=');
+    const shopId = linkParts[linkParts.length - 1]; // Last part of the link is the shop id.
+
+    const accountLink = document.createElement('a');
+    accountLink.href = 'https://account.findologic.io/#/' + shopId;
+    accountLink.classList.add('btn');
+    accountLink.innerText = 'ACCOUNT ➡';
+    accountLink.setAttribute('target', '_blank');
+
+    const upperButtonContainer = document.querySelector('.well.pull-right.upper-buttons');
+    upperButtonContainer.appendChild(accountLink);
+})();
